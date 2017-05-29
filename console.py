538e6d77711020c6102da73e0c53fab0de3c4eb1
Steven Lindberg <stevenjohnlindberg@gmail.com>
2017-05-28 20:33:58 -0700

    Fixed chmodding on darwin

--- console.py
+++ console.py
@@ -4,15 +4,16 @@ import sys
 import platform
 import serial
 
-#subprocess.Popen("sudo chmod 777 ")
-print "sudo chmod 777 /dev/ttyUSB0", sys.argv[0]
-
 os = platform.system()
 
 devices = {
     'Darwin': '/dev/tty.SLAB_USBtoUART',
     'Linux': '/dev/ttyUSB0',
 }
+
+# To write to the serial port, we have to have write permission on the device
+if os == 'Linux':
+    print "sudo chmod 777 /dev/ttyUSB0", sys.argv[0]
 
 port = serial.Serial(devices[os], baudrate=9600, timeout=3.0)
 
